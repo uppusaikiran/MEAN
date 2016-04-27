a63c4b1ad13e79b88759a570770441efbc9de470
uppusaikiran <saikiran.vit@gmail.com>
2016-04-27 21:33:55 +0530

    broadcast socket.io

--- socket.io/socket/server.js
+++ socket.io/socket/server.js
@@ -4,5 +4,14 @@ var io = require('socket.io')(app)
 app.listen(8000)
 
 io.on('connection',function(socket){
-	socket.emit('alert','hello from server')
+	var alert = 'hello from server ' +Math.random();
+	socket.emit('alert',alert)
+
+	socket.on('click',function (data) {
+		console.log(data)
+	})
+
+	socket.on('disconnect',function () {
+		console.log('1 client disconnected')
+	})
 })
