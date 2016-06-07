e8f24872353dd8abd3d69c4267e9fce76988974c
uppusaikiran <saikiran.vit@gmail.com>
2016-06-07 13:08:22 +0530

    "added component"

--- Angular-examples/Angular2.0/angular2-seed/app/app.component.ts
+++ Angular-examples/Angular2.0/angular2-seed/app/app.component.ts
@@ -1,7 +1,9 @@
 import {Component} from 'angular2/core';
+import {CoursesComponent} from './courses.component'
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Hello AngularJS</h1>'
+    template: '<h1>Hello AngularJS</h1><courses></courses>' ,
+    directives : [CoursesComponent]
 })
 export class AppComponent { }
